89e4514e72b45c81e52df69805541812db6c7348
Ric <rlahura@icloud.com>
2022-09-26 17:44:23 -0500

    Displaying with cors 4

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+ res.send({
+  token: 'test123',
+ });
+});
+
+app.listen(process.env.PORT || 8080, () =>
+ console.log('API is running on http://localhost:8080/login')
+);
